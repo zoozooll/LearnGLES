83905737ca01c6a234bd667c1aaa133c22c80597
Aaron Lee <kangkang365@gmail.com>
2023-07-11 13:41:42 +0800

    Update tutorial list

--- app/src/main/java/com/example/learngles/GLDetailActivity.kt
+++ app/src/main/java/com/example/learngles/GLDetailActivity.kt
@@ -2,10 +2,19 @@ package com.example.learngles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.learngles.databinding.ActivityGldetailBinding
+
 
 class GLDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGldetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gldetail)
+
+        binding = ActivityGldetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val tutorialArg = intent.getStringExtra(MainActivity.KEY_TUTORIAL_TITLE)
+        Log.i("GLDetailActivity", "tutorialArg $tutorialArg")
     }
 }
--- app/src/main/java/com/example/learngles/MainActivity.kt
+++ app/src/main/java/com/example/learngles/MainActivity.kt
@@ -6,54 +6,71 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.learngles.MainActivity.ContactsAdapter.Companion.TYPE_GROUP
+import com.example.learngles.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listAdapter: ContactsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.root.layoutManager = LinearLayoutManager(this@MainActivity)
+        listAdapter = ContactsAdapter()
+        binding.root.adapter = listAdapter
     }
 
 
 
-    inner class ContactsAdapter () : RecyclerView.Adapter<ContactsAdapter.ViewHolder>()
+    inner class ContactsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
     {
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        inner class GroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView = itemView.findViewById<TextView>(android.R.id.text1)
+            init {
+                itemView.setBackgroundResource(android.R.color.holo_purple)
+            }
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
-            val context = parent.context
-            val inflater = LayoutInflater.from(context)
-            val contactView = when(viewType) {
-                TYPE_GROUP -> {
-                    inflater.inflate(android.R.layout.simple_list_item_1, parent, false).apply {
-                        setOnClickListener(null)
-                    }
-                }
-                else -> {
-                    inflater.inflate(android.R.layout.simple_list_item_2, parent, false).apply {
-                        setOnClickListener {
-                            startActivity(Intent(this@MainActivity, GLDetailActivity::class.java).apply {
-                                putExtra(KEY_TUTORIAL_TITLE, this)
-                            })
-                        }
-                    }
+        inner class TutorialViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val text1 = itemView.findViewById<TextView>(android.R.id.text1)
+            val text2 = itemView.findViewById<TextView>(android.R.id.text2)
+            init {
+                itemView.setOnClickListener {
+                    val intent = Intent(this@MainActivity, GLDetailActivity::class.java)
+                    intent.putExtra(KEY_TUTORIAL_TITLE, text1.text.toString())
+                    startActivity(intent)
                 }
             }
-            return ViewHolder(contactView)
         }
 
-        override fun onBindViewHolder(viewHolder: ContactsAdapter.ViewHolder, position: Int) {
-            val contact: TutorialBean = mContacts.get(position)
-            val textView = viewHolder.nameTextView
-            textView.setText(contact.name)
-            val button = viewHolder.messageButton
-            button.text = if (contact.isOnline) "Message" else "Offline"
-            button.isEnabled = contact.isOnline
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val context = parent.context
+            val inflater = LayoutInflater.from(context)
+            return when(viewType) {
+                TYPE_GROUP -> {
+                    val itemView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+                    GroupViewHolder(itemView)
+                }
+                else -> {
+                    val itemView = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+                    TutorialViewHolder(itemView)
+                }
+            }
+        }
+
+        override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+            if (viewHolder is GroupViewHolder) {
+                viewHolder.textView.text = DataSet.initData[position]
+            } else if (viewHolder is TutorialViewHolder) {
+                viewHolder.text1.text = DataSet.initData[position]
+            }
         }
 
         override fun getItemCount(): Int {
@@ -61,7 +78,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getItemViewType(position: Int): Int {
-            val item = DataSet.initData[position];
+            val item = DataSet.initData[position]
             return if (item[0].isDigit()) {
                 TYPE_GROUP
             } else {
