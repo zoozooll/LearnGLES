dba4950ce312266ff4a0b6b3b5dc558f6fd85f4f
Aaron Lee <kangkang365@gmail.com>
2023-07-24 11:32:00 +0800

    Update HelloTriangleExercise3Scene.cpp

--- app/src/main/cpp/getting_started/HelloTriangleExercise3Scene.cpp
+++ app/src/main/cpp/getting_started/HelloTriangleExercise3Scene.cpp
@@ -8,19 +8,19 @@
 
 #include "logutil.h"
 
-static const char *vertexShaderSource = "#version 330 core\n"
+static const char *vertexShaderSource = "#version 320 es\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
-static const char *fragmentShader1Source = "#version 330 core\n"
+static const char *fragmentShader1Source = "#version 320 es\n"
                                     "out vec4 FragColor;\n"
                                     "void main()\n"
                                     "{\n"
                                     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
                                     "}\n\0";
-static const char *fragmentShader2Source = "#version 330 core\n"
+static const char *fragmentShader2Source = "#version 320 es\n"
                                     "out vec4 FragColor;\n"
                                     "void main()\n"
                                     "{\n"
