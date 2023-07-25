f338c492854903bb38257826ca071ff3f811f81a
Aaron Lee <kangkang365@gmail.com>
2023-07-25 15:41:29 +0800

    Update SceneHelper.h

--- app/src/main/cpp/SceneHelper.h
+++ app/src/main/cpp/SceneHelper.h
@@ -11,6 +11,19 @@
 #include "getting_started/HelloWindowScene.h"
 #include "getting_started/HelloWindowClearScene.h"
 #include "getting_started/HelloTriangleScene.h"
+#include "getting_started/HelloTriangleIndexedScene.h"
+#include "getting_started/HelloTriangleExercise1Scene.h"
+#include "getting_started/HelloTriangleExercise2Scene.h"
+#include "getting_started/HelloTriangleExercise3Scene.h"
+#include "getting_started/ShadersUniformScene.h"
+#include "getting_started/ShadersInterpolationScene.h"
+#include "getting_started/ShadersClassScene.h"
+#include "getting_started/ShadersExercise1Scene.h"
+#include "getting_started/ShadersExercise2Scene.h"
+#include "getting_started/ShadersExercise3Scene.h"
+#include "getting_started/TexturesScene.h"
+#include "getting_started/TextureCombinedScene.h"
+#include "getting_started/TransformationsScene.h"
 
 inline Scene* GenSceneByName(const char *tutorial) {
     if (!strcmp(tutorial, "hello_window")) {
@@ -20,31 +33,31 @@ inline Scene* GenSceneByName(const char *tutorial) {
     } else if (!strcmp(tutorial, "hello_triangle")) {
         return new HelloTriangleScene;
     } else if (!strcmp(tutorial, "hello_triangle_indexed")) {
-
+        return new HelloTriangleIndexedScene;
     } else if (!strcmp(tutorial, "hello_triangle_exercise1")) {
-
+        return new HelloTriangleExercise1Scene;
     } else if (!strcmp(tutorial, "hello_triangle_exercise2")) {
-
+        return new HelloTriangleExercise2Scene;
     } else if (!strcmp(tutorial, "hello_triangle_exercise3")) {
-
+        return new HelloTriangleExercise3Scene;
     } else if (!strcmp(tutorial, "shaders_uniform")) {
-
+        return new ShadersUniformScene;
     } else if (!strcmp(tutorial, "shaders_interpolation")) {
-
+        return new ShadersInterpolationScene;
     } else if (!strcmp(tutorial, "shaders_class")) {
-
+        return new ShadersClassScene;
     } else if (!strcmp(tutorial, "shaders_exercise1")) {
-
+        return new ShadersExercise1Scene;
     } else if (!strcmp(tutorial, "shaders_exercise2")) {
-
+         return new ShadersExercise2Scene;
     } else if (!strcmp(tutorial, "shaders_exercise3")) {
-
+         return new ShadersExercise3Scene;
     } else if (!strcmp(tutorial, "textures")) {
-
+        return new TexturesScene;
     } else if (!strcmp(tutorial, "texture_combined")) {
-
+        return new TextureCombinedScene;
     } else if (!strcmp(tutorial, "transformations")) {
-
+        return new TransformationsScene;
     } else if (!strcmp(tutorial, "transformations_exercise1")) {
 
     } else if (!strcmp(tutorial, "transformations_exercise2")) {
