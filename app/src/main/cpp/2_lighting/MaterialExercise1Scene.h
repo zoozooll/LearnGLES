bd971a7c1b23e5e142e48b2faa7cf20a79c770b0
aaronlee <kangkang365@gmail.com>
2023-09-19 20:26:09 +0800

    Add material exercise

--- app/src/main/cpp/2_lighting/MaterialExercise1Scene.h
+++ app/src/main/cpp/2_lighting/MaterialExercise1Scene.h
@@ -6,6 +6,10 @@
 #define LEARNGLES_MATERIALEXERCISE1SCENE_H
 
 #include "Scene.h"
+
+#include "Scene.h"
+#include "Camera.h"
+class Shader;
 class MaterialExercise1Scene : public Scene {
 public:
     void init() override;
@@ -15,6 +19,16 @@ public:
     void draw() override;
 
     void destroy() override;
+private:
+    unsigned int VBO, cubeVAO;
+    unsigned int lightCubeVAO;
+    Shader *lightingShader = nullptr;
+    Shader *lightCubeShader = nullptr;
+
+    glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
+    Camera camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
+    unsigned int SCR_WIDTH = 0;
+    unsigned int SCR_HEIGHT = 0;
 };
 
 
